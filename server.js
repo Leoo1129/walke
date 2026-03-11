1a456ac1626052a2b8a2dd209744130c88d2aefe
Teammate A <teammate-a@example.com>
2026-03-11 16:16:36 +1100

    made server.js use better software architecture

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express")
 
+const orderController = require("./controllers/orderController")
+const productController = require("./controllers/productController")
+const userController = require("./controllers/userController")
+const voucherController = require("./controllers/voucherController")
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -12,10 +17,12 @@ app.use(function(req, res, next)
     next()
 })
 
+
 app.get("/", function(req, res)
 {
     res.send("Walke API running")
 })
+
 
 app.get("/health", function(req, res)
 {
@@ -27,54 +34,22 @@ app.get("/health", function(req, res)
 })
 
 
+app.post("/orders", orderController.createOrder)
 
-app.post("/orders", function(req, res)
+app.post("/products", productController.createProduct)
+app.get("/products", productController.getProducts)
+
+app.post("/users", userController.createUser)
+
+app.post("/vouchers", voucherController.createVoucher)
+
+
+app.use(function(req, res)
 {
-    const body = req.body
-
-    if (!body.buyer_id)
-    {
-        return res.status(400).json({
-            error: "buyer_id required"
-        })
-    }
-
-    if (!Array.isArray(body.items) || body.items.length === 0)
-    {
-        return res.status(400).json({
-            error: "items array required"
-        })
-    }
-
-    for (const item of body.items)
-    {
-        if (!item.product_id || !item.quantity)
-        {
-            return res.status(400).json({
-                error: "each item must include product_id and quantity"
-            })
-        }
-    }
-
-
-
-    const order_id = 69
-
-    const order = {
-        id: order_id,
-        buyer_id: body.buyer_id,
-        voucher_id: body.voucher_id || null,
-        status: "pending",
-        created_at: new Date(),
-        items: body.items
-    }
-
-    res.status(201).json({
-        message: "order created",
-        order: order
+    res.status(404).json({
+        error: "route not found"
     })
 })
-
 
 
 app.listen(PORT, function()
