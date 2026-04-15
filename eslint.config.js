b6aa0de9a7a5d6035d672502f22fe3178d83e8f2
Teammate A <teammate-a@example.com>
2026-04-15 11:37:31 +1000

    added front end to accomodate business features

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,9 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
+    { ignores: ['node_modules/**', 'client/**'] },
     js.configs.recommended,
     {
-        ignores: ['node_modules/**', 'client/**'],
         languageOptions: {
             globals: {
                 ...globals.node,
